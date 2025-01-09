e2d55d8d7efa01154707381ac4412ad481d5278b
Shruti Varpe <shrutivarpe193@gmail.com>
2025-01-09 14:24:00 +0530

    Variables in java

---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+    public static void main(String[] args) {
+//        MY Variables
+        float myfloat = 5.99f;
+        Boolean mybool = true;
+        char myletter = 'S';
+        String mystr = "Shruti";
+        int myint = 123;
+        System.out.println(myfloat);
+        System.out.println(mybool);
+        System.out.println(myletter);
+        System.out.println(mystr);
+        System.out.println(myint);
+
+    }
+
+}
